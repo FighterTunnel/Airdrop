75a2210891b5202ed28c4af50eb177ff88f33929
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2025-02-11 13:27:56 +0700

    Create auto.sh

---
+++ nodego/auto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/airdropinsiders/NodeGo-Auto-Bot.git
+cd NodeGo-Auto-Bot
+npm install
+rm /root/NodeGo-Auto-Bot/data.txt
+wget https://raw.githubusercontent.com/FighterTunnel/Airdrop/refs/heads/main/nodego/data.txt
+tmux new-session -d -s "NodeGo-Auto-Bot" "npm run start"

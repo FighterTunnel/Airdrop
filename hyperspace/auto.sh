9407d1cf5eceb4abdd6c2cc632a9c2cc8dcadf96
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2025-02-14 01:20:00 +0700

    Create auto.sh

---
+++ hyperspace/auto.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt update -y
+curl https://download.hyper.space/api/install | bash
+source /root/.bashrc
+
+wget https://raw.githubusercontent.com/FighterTunnel/Airdrop/refs/heads/main/hyperspace/my.pem
+chmod 600 my.pem
+tmux new-session -d -s "hyperspace" "aios-cli start"
+
+aios-cli models add hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf
+aios-cli hive import-keys ./my.pem
+aios-cli hive login
+aios-cli hive select-tier 5
+aios-cli infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "Can you explain the concept of hyperspace and its applications in science fiction?"
+aios-cli hive infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "Hello, airdropnode! Can you explain hyperspace and its connection to modern science?"
+aios-cli hive login
+aios-cli hive connect

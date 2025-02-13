ac8565d225ad26e76effc7882912d7b062e97f70
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2025-02-14 01:58:51 +0700

    Create ewe.sh

---
+++ hyperspace/ewe.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Validate and read private key content
+read -p "Please input the content of PKey: " pem_content
+echo "$pem_content" > my.pem
+apt update -y
+curl https://download.hyper.space/api/install | bash
+cp /root/.aios/aios-cli /usr/bin/aios-cli
+
+
+chmod 600 my.pem
+tmux new-session -d -s "hyperspace" "aios-cli start"
+sleep 10
+
+aios-cli models add hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf
+aios-cli hive import-keys ./my.pem
+aios-cli hive login
+aios-cli hive select-tier 5
+aios-cli infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "Can you explain the concept of hyperspace and its applications in science fiction?"
+aios-cli hive infer --model hf:TheBloke/phi-2-GGUF:phi-2.Q4_K_M.gguf --prompt "Hello, airdropnode! Can you explain hyperspace and its connection to modern science?"
+aios-cli hive login
+aios-cli hive connect

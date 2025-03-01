7e7437bab971991e1f99995dd9967b4067b86709
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2025-03-02 02:28:31 +0700

    Update multiple.sh

--- node-multiple/multiple.sh
+++ node-multiple/multiple.sh
@@ -18,14 +18,14 @@ download_file() {
     fi
 
     echo "Mendownload file $file_name..."
-    wget -q $url -O $file_name
+    wget -q "$url" -O "$file_name"
 }
 
 # Fungsi untuk mengekstrak file tar
 extract_tar() {
     local file_name=$1
     echo "Mengekstrak file $file_name..."
-    tar -xvf $file_name
+    tar -xvf "$file_name"
 }
 
 # Memeriksa arsitektur sistem
@@ -33,7 +33,7 @@ ARCHITECTURE=$(uname -m)
 
 # Mengatur URL sesuai arsitektur
 if [ "$ARCHITECTURE" == "x86_64" ]; then
-    CLIENT_URL="wget https://mdeck-download.s3.us-east-1.amazonaws.com/client/linux/MultipleForLinux.tar"
+    CLIENT_URL="https://mdeck-download.s3.us-east-1.amazonaws.com/client/linux/MultipleForLinux.tar"
     CLIENT_FILE="MultipleForLinux.tar"
 elif [ "$ARCHITECTURE" == "aarch64" ]; then
     CLIENT_URL="https://cdn.app.multiple.cc/client/linux/arm64/multipleforlinux.tar"
@@ -44,15 +44,15 @@ else
 fi
 
 # Mengunduh file client
-download_file $CLIENT_URL $CLIENT_FILE
+download_file "$CLIENT_URL" "$CLIENT_FILE"
 
 # Mengekstrak file yang telah diunduh
-extract_tar $CLIENT_FILE
+extract_tar "$CLIENT_FILE"
 
 # Hapus file .tar setelah ekstraksi
 if [ -f "$CLIENT_FILE" ]; then
     echo "Menghapus file tar setelah ekstraksi..."
-    rm -f $CLIENT_FILE
+    rm -f "$CLIENT_FILE"
 fi
 
 # Memastikan direktori multipleforlinux ada setelah ekstraksi
@@ -62,7 +62,7 @@ if [ ! -d "./multipleforlinux" ]; then
 fi
 
 # Pindah ke direktori multipleforlinux
-cd ./multipleforlinux
+cd ./multipleforlinux || { echo "Gagal masuk ke direktori multipleforlinux"; exit 1; }
 
 # Memeriksa apakah file multiple-cli dan multiple-node ada setelah ekstraksi
 if [ ! -f "./multiple-cli" ] || [ ! -f "./multiple-node" ]; then
@@ -80,12 +80,11 @@ echo "Menambahkan direktori ke PATH: $EXTRACTED_DIR"
 export PATH=$PATH:$EXTRACTED_DIR
 
 # Memberikan izin penuh pada folder hasil ekstraksi
-chmod -R 777 $EXTRACTED_DIR
+chmod -R 777 "$EXTRACTED_DIR"
 
 # Menjalankan aplikasi dengan output log
 echo "Menjalankan program multiple-node..."
 nohup ./multiple-node > output.log 2>&1 &
-
 
 # Menjalankan bind dengan parameter yang dimasukkan
 ./multiple-cli bind --bandwidth-download 100 --identifier 1S00BIIS --pin 123369 --storage 200 --bandwidth-upload 100
